9b48675c72953738540405aa165310adad4f2907
Felipe Augusto dos Santos Silva <feaugustosantossilva@gmail.com>
2025-03-24 08:02:22 -0300

    Correcao

--- 1º Período/Algoritmos de programação/whileIf/P04Ex06.py
+++ 1º Período/Algoritmos de programação/whileIf/P04Ex06.py
@@ -1,4 +1,7 @@
 S = 0
 for i in range(1,11,2):
-    S += (i / i**2) - ((i + 1) / (i + 1) / i** 2)
+    if i % 2 == 0:
+        S -= (i / i**2)  
+    else:
+        S += (i / i**2)
 print(f'{S : .3f}')
--- 1º Período/Algoritmos de programação/whileIf/P04Ex09.py
+++ 1º Período/Algoritmos de programação/whileIf/P04Ex09.py
@@ -1,15 +1,17 @@
-cid = int(input('Entre com o número de cidadãos: '))
+sal = 0
+cid = 0
 sSal = 0
 sFil = 0
 mSal = -999999
 cont = 0
-for i in range(0, cid):
-    sal = float(input(f'Entre o salário da {i + 1}ª pessoa: '))
-    fil = int(input(f'Entre a quantidade de filho da {i + 1}ª pessoa: '))
+while sal >= 0:
+    sal = float(input(f'Entre o salário: '))
+    fil = int(input(f'Entre a quantidade de filho: '))
     sSal += sal
     sFil += fil
     if sal > mSal:
         mSal = sal
     if sal < 100:
         cont += 1
+    cid += 1
 print(f'\n\ta. {sSal / cid}\n\tb. {sFil / cid}\n\tc. {mSal}\n\td. {(cont / cid) * 100}%')
