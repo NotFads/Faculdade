65a0d42700ce534737fb3e39b749fbcb5dda7f93
Felipe Augusto dos Santos Silva <feaugustosantossilva@gmail.com>
2025-03-26 20:30:23 -0300

    Correção P04Ex08.py

--- 1º Período/Algoritmos de programação/whileIf/P04Ex08.py
+++ 1º Período/Algoritmos de programação/whileIf/P04Ex08.py
@@ -1,9 +1,14 @@
 N = int(input('Entre com um número inteiro: '))
-S = 0
-
-a, b = 2, 3  
-
-for i in range(2, N + 2):  
-    print(a, end=" ")
-    a, b = b, a + b
-
+anterior1 = 3
+anterior2 = 2
+x = 0
+S = 5
+print(f'{anterior2} {anterior1}', end=" ")
+for i in range(1, N - 1):
+    aux = anterior1
+    x = anterior1 + anterior2
+    S += x
+    anterior1 = x
+    anterior2 = aux
+    print(x, end=" ")
+print(f'= {S}')
