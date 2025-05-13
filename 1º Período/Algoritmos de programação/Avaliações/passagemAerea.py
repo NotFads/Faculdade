02548685b865ab25954c731d30217adcb6dcde85
Felipe Augusto dos Santos Silva <feaugustosantossilva@gmail.com>
2025-05-12 23:28:41 -0300

    passagemAerea atualizada

--- 1º Período/Algoritmos de programação/Avaliações/passagemAerea.py
+++ 1º Período/Algoritmos de programação/Avaliações/passagemAerea.py
@@ -1,12 +1,12 @@
 programaAtivo = 1
 voos = dict()
 passageiros = dict()
+disp = []
+comp = []
 
 while programaAtivo == 1:
     menu = int(input(f'-------Menu-------\n1.Cadastrar Voo.\n2. Consultar um Voo.\n3. Informar o Voo com Menor Escala.\n4. Listar Passageiros de um Voo.\n5. Venda de Passagem.\n6. Cancelamento de Passagem.\n0. Encerrar programa.'))
-    #Cadastro de Voo
-    disp = []
-    comp = []
+    menorE = 999
     if menu == 1:
         nVoo = int(input('Entre com o número do voo: '))
         cidadeO = input('Entre com a cidade de origem: ')
@@ -14,26 +14,58 @@ while programaAtivo == 1:
         nEscala = int(input('Entre com o número de escalas: '))
         preco = float(input('Entre com o preço da passagem: '))
         lugares = int(input('Entre com o número de lugares: '))
-        disp.append(lugares)
         voos[nVoo] = [cidadeO,cidadeD,nEscala,preco,lugares]
+        if lugares > 0:
+            disp.append(voos[nVoo])
     elif menu == 2:
         consulta = int(input(f'Tipos de Consulta\n1. Código do voo.\n2. Cidade Origem.\n3. Cidade Destino.'))
         if consulta == 1:
             codigo = int(input('Entre o código que você deseja buscar: '))
-            for codigo in voos.keys():
-                print(voos)
+            if codigo in voos.keys():
+                print(voos[codigo])
+            else:
+                print('Voo não encontrado.')
         elif consulta == 2:
             cidadeOB = input('Entre a cidade de origem que você deseja buscar: ')
-            for origem in voos.values():
-                if voos[0] == cidadeOB:
-                    print(voos)
-
+            encontrado = 0
+            for voo in voos.values():
+                if voo[0].lower() == cidadeOB.lower():
+                    print(voo)
+                    encontrado = 1
+            if encontrado == 0:
+                print('Voo não encontrado.')
         elif consulta == 3:
             cidadeDB = input('Entre com a cidade de destino que você deseja buscar: ')
-            for destino in voos.values():
-                encontrado = 0
-                if voos[1] == cidadeDB:
-                    print(voos)
+            encontrado = 0
+            for voo in voos.values():
+                if voo[1].lower() == cidadeDB.lower():
+                    print(voo)
                     encontrado = 1
-                if encontrado == 0:
-                    print('Voo não encontrado.')
+            if encontrado == 0:
+                print('Voo não encontrado.')
+    elif menu == 3:
+        #Achar um jeito de printar só uma vez
+        cidadeOB = input('Entre a cidade de origem que você deseja: ')
+        cidadeDB = input('Entre a cidade de destino que você deseja: ')
+        for voo in voos.values():
+            if voo[0].lower() == cidadeOB.lower() and voo[1].lower() == cidadeDB.lower():
+                if voo[2] < menorE:
+                    menorE = voo[2]
+                    print(voo)
+    #elif menu == 4:
+    elif menu == 5:
+        for i in range(len(disp)):
+            print(disp[i])
+        dispB = int(input('Entre o voo que você está interessado: '))
+        cpf = input('Entre com o seu CPF: ')
+        nome = input('Entre com o seu nome: ')
+        tel = int(input('Entre com o seu número de telefone: '))
+        passagem = int(input('Entre com o número de passagens que você deseja comprar: '))
+        passageiros[cpf] = [nome,tel]
+        print(passageiros)
+        for voo in voos:
+            if voo == disp[dispB]:
+                voo[4] -= passagem
+                disp[dispB][4] -= passagem
+                
+
